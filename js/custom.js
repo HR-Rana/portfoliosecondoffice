8a53aae4256a1020c72ef6d45d37214acff350f7
webworld420 <76555387+webworld420@users.noreply.github.com>
2021-05-18 11:23:28 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        100:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
+});
